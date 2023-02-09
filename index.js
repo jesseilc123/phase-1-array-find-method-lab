3768a7495b10bccb84ada2a0fb1b5bd5e66866bb
Jesse Ilc <jesseilc123@gmail.com>
2023-02-09 11:15:42 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(array){
+    const win = array.find(function (item){
+            return item.result === "W"
+    })
+    if(win !== undefined){
+        return win.year
+    }else{
+        return undefined
+    };
+};
